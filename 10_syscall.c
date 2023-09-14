87a0496f9cd578cf1684c4f6dc625f68b304217b
IgorAmashukeli <amashukeliim1150@gmail.com>
2023-09-14 18:36:39 +0100

    syscall

--- 10_syscall.c
+++ 10_syscall.c
@@ -28,13 +28,16 @@ enum {
 
 
 #define unreachable()\
-    abort();
+    while(1) {};
 
 
 
 
 
 /**check return value from system call, set value for a C function error**/
+
+// using int everywhere, cause ssize_t is signed long
+// which is int on x86_64 linux and pid_t is also int
 int set_ret(int ret) {
 
     // return value is in correct range for error
@@ -67,15 +70,15 @@ int open(const char* pathname, int flags) {
     // "volatile" is needed here, because of Output Operands
     asm volatile (
 
-            // system call
+        // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // pathname is put in %rdi
@@ -88,7 +91,7 @@ int open(const char* pathname, int flags) {
             "d"(mode)
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -96,7 +99,7 @@ int open(const char* pathname, int flags) {
 
             // %rcx and %r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // set return value and errno according to x86_64 system call conventions
     return set_ret(ret);
@@ -116,15 +119,15 @@ ssize_t read(int fd, void* buf, size_t count) {
     // "volatile" is needed here, because of Output Operands
     asm volatile (
 
-            // system call
+        // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // fd is put in %rdi
@@ -137,7 +140,7 @@ ssize_t read(int fd, void* buf, size_t count) {
             "d"(count)
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -145,7 +148,7 @@ ssize_t read(int fd, void* buf, size_t count) {
 
             // %rcx and r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // set return value and errno according to x86_64 system call conventions
     return set_ret(ret);
@@ -165,15 +168,15 @@ ssize_t write(int fd, const void *buf, ssize_t count) {
     // "volatile" is needed here, because of Output Operands
     asm volatile (
 
-            // system call
+        // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // fd is put in %rdi
@@ -186,7 +189,7 @@ ssize_t write(int fd, const void *buf, ssize_t count) {
             "d"(count)
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -194,7 +197,7 @@ ssize_t write(int fd, const void *buf, ssize_t count) {
 
             // %rcx and r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // set return value and errno according to x86_64 system call conventions
     return set_ret(ret);
@@ -214,22 +217,22 @@ int pipe(int pipefd[2]) {
     // "volatile" is needed here, because of Output Operands
     asm volatile (
 
-            // system call
+        // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // pipefd is put in %rdi
             "D"(pipefd)
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -237,7 +240,7 @@ int pipe(int pipefd[2]) {
 
             // %rcx and r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // set return value and errno according to x86_64 system call conventions
     return set_ret(ret);
@@ -257,22 +260,22 @@ int close(int fd) {
     // "volatile" is needed here, because of Output Operands
     asm volatile (
 
-            // system call
+        // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // fd is put in %rdi
             "D"(fd)
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -280,7 +283,7 @@ int close(int fd) {
 
             // %rcx and r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // set return value and errno according to x86_64 system call conventions
     return set_ret(ret);
@@ -299,22 +302,22 @@ int dup(int oldfd) {
     // "volatile" is needed here, because of Output Operands
     asm volatile (
 
-            // system call
+        // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // oldfd is put in %rdi
             "D"(oldfd)
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -322,7 +325,7 @@ int dup(int oldfd) {
 
             // %rcx and r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // set return value and errno according to x86_64 system call conventions
     return set_ret(ret);
@@ -339,22 +342,22 @@ void exit(int status) {
     // there is no need for "volatile" here, because of no Output Operands
     asm (
 
-            // system call
+        // system call
             "syscall\n\t"
 
-            // there are no Output Operands
+        // there are no Output Operands
             :
 
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // status is put in %rdi
             "D"(status)
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -362,7 +365,7 @@ void exit(int status) {
 
             // %rcx and r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // nothing is returned from exit
     // it is guaranteed by exit syscall
@@ -370,7 +373,7 @@ void exit(int status) {
     // to show there is no return
     // cause compiler thinks we are returning from no-return function here
     // therefore, we can use endless loop, for example
-    no_return_code();
+    unreachable();
 }
 
 
@@ -386,21 +389,21 @@ pid_t fork(void) {
     // "volatile" is needed here, because of Output Operands
     asm volatile (
 
-            // system call
+        // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number)
 
-            // no arguments in this system call
+        // no arguments in this system call
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -408,7 +411,7 @@ pid_t fork(void) {
 
             // %rcx and r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // set return value and errno according to x86_64 system call conventions
     return set_ret(ret);
@@ -430,18 +433,18 @@ pid_t waitpid(pid_t pid, int *wstatus, int options) {
     // inline assembly
     // "volatile" is needed here, because of Output Operands
     asm volatile(
-            // move r10, because there are no "%r10" constraints
+        // move r10, because there are no "%r10" constraints
             "movq %[r10_content], %%r10\n\t"
 
             // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // pid is put in %rdi
@@ -453,15 +456,15 @@ pid_t waitpid(pid_t pid, int *wstatus, int options) {
             // options is put in %rdx
             "d"(options),
 
-            // struct rusage* ru argument
-            // there is no "r10" constraint
-            // compiler should put this argument in some register/memory
-            // then inline code moves it to "r10"
-            [r10_content] "rm"(ru)
+    // struct rusage* ru argument
+    // there is no "r10" constraint
+    // compiler should put this argument in some register/memory
+    // then inline code moves it to "r10"
+    [r10_content] "rm"(ru)
 
-            :
-            // system call can change memory
-            "memory",
+    :
+    // system call can change memory
+    "memory",
 
             // input/output operands can change flags
             "cc",
@@ -489,15 +492,15 @@ int execve(const char *filename, char *const argv[], char *const envp[]) {
     // "volatile" is needed here because of Output Operands
     asm volatile (
 
-            // system call
+        // system call
             "syscall\n\t"
 
 
-            // %rax will contain return value
+        // %rax will contain return value
             : "=a"(ret)
 
             :
-            // number of the syscall is put in "%rax"
+        // number of the syscall is put in "%rax"
             "a"(number),
 
             // filename is put in %rdi
@@ -510,7 +513,7 @@ int execve(const char *filename, char *const argv[], char *const envp[]) {
             "d"(envp)
 
             :
-            // system call can change memory
+        // system call can change memory
             "memory",
 
             // input/output operands can change flags
@@ -518,7 +521,7 @@ int execve(const char *filename, char *const argv[], char *const envp[]) {
 
             // %rcx and r11 are clobbered in x86_64 system call
             "rcx", "r11"
-    );
+            );
 
     // set return value and errno according to x86_64 system call conventions
     return set_ret(ret);
